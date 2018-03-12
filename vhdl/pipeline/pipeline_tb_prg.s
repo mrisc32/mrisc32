b4f86b790ebfc99aba00c78a96262ceeb0f42293
m <m@bitsnbites.eu>
2018-03-13 00:34:10 +0100

    More interesting test program

--- vhdl/pipeline/pipeline_tb_prg.s
+++ vhdl/pipeline/pipeline_tb_prg.s
@@ -2,26 +2,39 @@
 ;
 ; This program is compiled using asm.py, and loaded into the testbench.
 
+boot:
+    ; Start by setting up the stack.
+    LDI   SP, 0x00010000  ; We grow down from 64KB.
+
 main:
-  LDI   S1,0x1234
-  LDI   S2,0x1111
+    LDI   S9, 4           ; Loop count.
+    LDI   S1, 0x1234
+    LDI   S2, 0x1111
 .loop:
-  BEQ   S2,.dont_go_here
-  BNE   Z,.dont_go_here
-  ADD   S5,S2,S2
-  BL    .subroutine
-  ADD   S3,S1,S2
-  SUB   S4,S1,S2
-  ADD   S1,S1,1
-  B     .loop
+    BEQ   S1, .dont_go_here
+    BNE   Z, .dont_go_here
+    ADD   S5, S1, S1
+    BL    .subroutine
+    ADD   S3, S1, S2
+    SUB   S4, S1, S2
+    ADD   S1, S1, 1
+    ADD   S9, S9, -1
+    BNE   S9, .loop
+    B     main
 
 .dont_go_here:
-  OR    S1,Z,0xBAD  ; Should not be executed.
+    OR    S1, Z, 0xBAD    ; Should not be executed.
+    B     .dont_go_here
 
 .subroutine:
-  LDI   S9,0x34543
-  ADD   S9,S9,5
-  ADD   S9,S9,-4
-  AND   S9,S9,0x00FF
-  J     LR          ; Return from subroutine
-
+    ADD   SP, SP, -8
+    STW   LR, SP, 0
+    STW   S9, SP, 4
+    LDI   S9, 0x34543
+    ADD   S9, S9, 5
+    ADD   S9, S9, -4
+    AND   S9, S9, 0x00FF
+    LDW   LR, SP, 0
+    LDW   S9, SP, 4
+    ADD   SP, SP, 8
+    J     LR              ; Return from subroutine
