2cb959e520a799b3f179c23907b70b9dbb2ef112
m <m@bitsnbites.eu>
2018-03-15 23:29:55 +0100

    Finish the mulu operation in fewer cycles

--- vhdl/muldiv/mulu.vhd
+++ vhdl/muldiv/mulu.vhd
@@ -18,7 +18,22 @@
 ----------------------------------------------------------------------------------------------------
 
 ----------------------------------------------------------------------------------------------------
--- Simple looping integer multiplier.
+-- This is a multi-cycle multiplier for unsigned integers.
+--
+-- * The multiplication operation is (re)started when i_start_op is high.
+-- * The oparands i_src_a and i_src_b are read (must be defined) when i_start_op is high.
+-- * When the operation is done, o_result_ready is high during one cycle.
+--
+-- The looping implementation takes N+2 cycles to complete, where N is the position of the most
+-- significant non-zero bit in i_src_b (N=0 if i_src_b is zero). Some examples are:
+--
+--                i_src_b             | Cycles
+--  ----------------------------------+--------
+--   00000000000000000000000000000000 |    2
+--   00000000000000000000000000000001 |    2
+--   00000000000000000000000000000011 |    3
+--   00000000000000000000000011110011 |    9
+--   10000000000000000000000000000000 |   33
 ----------------------------------------------------------------------------------------------------
 
 library ieee;
@@ -27,8 +42,7 @@ use ieee.numeric_std.all;
 
 entity mulu is
   generic(
-      WIDTH : positive := 32;
-      COUNTER_BITS : positive := 5  -- Must be able to hold WIDTH-1
+      WIDTH : positive := 32
   );
   port(
       -- Control signals.
@@ -47,7 +61,7 @@ entity mulu is
       -- Outputs (async).
       o_next_result : out std_logic_vector(2*WIDTH-1 downto 0);
       o_next_result_ready : out std_logic
-    );
+  );
 end mulu;
 
 architecture rtl of mulu is
@@ -55,27 +69,32 @@ architecture rtl of mulu is
   signal s_src_a_wide : std_logic_vector(2*WIDTH-1 downto 0);
 
   -- Internal synchronous state.
-  signal s_sum : std_logic_vector(2*WIDTH-1 downto 0);
+  signal s_continue : std_logic;
+  signal s_result : std_logic_vector(2*WIDTH-1 downto 0);
   signal s_shifted_a : std_logic_vector(2*WIDTH-1 downto 0);
   signal s_shifted_b : std_logic_vector(WIDTH-1 downto 0);
-  signal s_counter : std_logic_vector(COUNTER_BITS-1 downto 0);
 
+  -- Intermediate results.
+  signal s_busy : std_logic;
   signal s_a_mul_b0 : std_logic_vector(2*WIDTH-1 downto 0);
-  signal s_sum_plus_a_mul_b0 : std_logic_vector(2*WIDTH-1 downto 0);
+  signal s_result_plus_a_mul_b0 : std_logic_vector(2*WIDTH-1 downto 0);
   signal s_a : std_logic_vector(2*WIDTH-1 downto 0);
   signal s_b : std_logic_vector(WIDTH-1 downto 0);
-  signal s_counter_minus_1 : std_logic_vector(COUNTER_BITS-1 downto 0);
+  signal s_only_zeros_left : std_logic;
 
-  signal s_next_sum : std_logic_vector(2*WIDTH-1 downto 0);
+  -- Internal state produced during this cycle.
+  signal s_next_continue : std_logic;
+  signal s_next_result : std_logic_vector(2*WIDTH-1 downto 0);
   signal s_next_shifted_a : std_logic_vector(2*WIDTH-1 downto 0);
   signal s_next_shifted_b : std_logic_vector(WIDTH-1 downto 0);
-  signal s_next_counter : std_logic_vector(COUNTER_BITS-1 downto 0);
   signal s_next_result_ready : std_logic;
 
-  constant C_ONE : unsigned(0 downto 0) := "1";
-  constant C_COUNTER_ZERO : std_logic_vector(COUNTER_BITS-1 downto 0) := (others => '0');
-  constant C_COUNTER_START : std_logic_vector(COUNTER_BITS-1 downto 0) := std_logic_vector(to_unsigned(WIDTH-1, COUNTER_BITS));
+  -- Constants.
+  constant C_B_ZERO : std_logic_vector(WIDTH-2 downto 0) := (others => '0');
 begin
+  -- Are we busy during this cycle?
+  s_busy <= i_start_op or s_continue;
+
   -- Widen the input signal.
   s_src_a_wide(WIDTH-1 downto 0) <= i_src_a;
   s_src_a_wide(2*WIDTH-1 downto WIDTH) <= (others => '0');
@@ -87,9 +106,9 @@ begin
   -- Multiply a by lowest bit of b.
   s_a_mul_b0 <= s_a when s_b(0) = '1' else (others => '0');
 
-  -- Update the sum.
-  s_sum_plus_a_mul_b0 <= std_logic_vector(unsigned(s_sum) + unsigned(s_a_mul_b0));
-  s_next_sum <= s_sum_plus_a_mul_b0 when i_start_op = '0' else s_a_mul_b0;
+  -- Update the result (may be intermediate or final).
+  s_result_plus_a_mul_b0 <= std_logic_vector(unsigned(s_result) + unsigned(s_a_mul_b0));
+  s_next_result <= s_result_plus_a_mul_b0 when i_start_op = '0' else s_a_mul_b0;
 
   -- Shift a to the left.
   s_next_shifted_a <= s_a(2*WIDTH-2 downto 0) & '0';
@@ -97,15 +116,15 @@ begin
   -- Shift b to the right.
   s_next_shifted_b <= '0' & s_b(WIDTH-1 downto 1);
 
-  -- Update the counter.
-  s_counter_minus_1 <= std_logic_vector(unsigned(s_counter) - C_ONE);
-  s_next_counter <= s_counter_minus_1 when i_start_op = '0' else C_COUNTER_START;
-
   -- Will the operation be finished after this cycle?
-  s_next_result_ready <= '1' when s_next_counter = C_COUNTER_ZERO else '0';
+  s_only_zeros_left <= '1' when s_b(WIDTH-1 downto 1) = C_B_ZERO else '0';
+  s_next_result_ready <= s_busy and s_only_zeros_left;
+
+  -- Should the loop continue during the next cycle?
+  s_next_continue <= s_busy and (not s_only_zeros_left);
 
   -- Asynchronous outputs.
-  o_next_result <= s_next_sum;
+  o_next_result <= s_next_result;
   o_next_result_ready <= s_next_result_ready;
 
   -- Synchronous output.
@@ -115,7 +134,7 @@ begin
       o_result <= (others => '0');
       o_result_ready <= '0';
     elsif rising_edge(i_clk) then
-      o_result <= s_next_sum;
+      o_result <= s_next_result;
       o_result_ready <= s_next_result_ready;
     end if;
   end process;
@@ -124,15 +143,15 @@ begin
   process(i_clk, i_rst)
   begin
     if i_rst = '1' then
-      s_sum <= (others => '0');
+      s_continue <= '0';
+      s_result <= (others => '0');
       s_shifted_a <= (others => '0');
       s_shifted_b <= (others => '0');
-      s_counter <= (others => '1');
     elsif rising_edge(i_clk) then
-      s_sum <= s_next_sum;
+      s_continue <= s_next_continue;
+      s_result <= s_next_result;
       s_shifted_a <= s_next_shifted_a;
       s_shifted_b <= s_next_shifted_b;
-      s_counter <= s_next_counter;
     end if;
   end process;
 end rtl;
