f030625cf5df58528aeb4c4d7c812a74856b461a
m <m@bitsnbites.eu>
2018-05-19 11:52:17 +0200

    Some changes to the Mandelbrot program

--- vhdl/pipeline/pipeline_tb_prg.s
+++ vhdl/pipeline/pipeline_tb_prg.s
@@ -8,19 +8,17 @@ boot:
 
 
 main:
-    LDI   S12, -2
-    LSL   S12, S12, 12    ; S12 = start_coord = -2.0
     LDI   S13, 64         ; S13 = coord_step  = 4.0 / 256 = 0.015625
     LDI   S17, 100        ; S17 = max_num_iterations
     LDI   S18, 16384      ; S18 = max_distance^2 = 4.0
 
     LDI   S14, 0x00008000 ; S14 = pixel_data (NOTE: must be after the program)
 
-    OR    S2, S12, Z      ; S2 = im(c) = -2.0
+    LDI   S2, -8192       ; S2 = im(c) = -2.0
     LDI   S16, 256        ; S16 = loop counter for y
 
 .outer_loop_y:
-    OR    S1, S12, Z      ; S1 = re(c) = -2.0
+    LDI   S1, -10240      ; S1 = re(c) = -2.5
     LDI   S15, 256        ; S15 = loop counter for x
 
 .outer_loop_x:
@@ -31,14 +29,14 @@ main:
 
 .inner_loop:
     MUL   S5, S3, S3
-    ASR   S5, S5, 12      ; S5 = re(z)^2
     MUL   S6, S4, S4
-    ASR   S6, S6, 12      ; S6 = im(z)^2
-
     MUL   S4, S3, S4
-    ASR   S4, S4, 11
-    ADD   S4, S4, S2      ; S4 = 2*re(z)*im(z) + im(c)
 
+    ASR   S5, S5, 12      ; S5 = re(z)^2
+    ASR   S6, S6, 12      ; S6 = im(z)^2
+    ASR   S4, S4, 11      ; S4 = 2*re(z)*im(z)
+
+    ADD   S4, S4, S2      ; S4 = 2*re(z)*im(z) + im(c)
     SUB   S3, S5, S6
     ADD   S3, S3, S1      ; S3 = re(z)^2 - im(z)^2 + re(c)
 
