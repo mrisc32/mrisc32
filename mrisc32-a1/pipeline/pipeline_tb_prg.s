4d3bf563ce33a07a15ab33782348d04551a4b2a5
m <m@bitsnbites.eu>
2018-09-10 23:12:44 +0100

    A1: Clear registers in pipeline_tb_prg.s
    The content of the registers is undefined after reset, so clearing the
    registers makes sure that we do not get metavalues or undefined behaviour.

--- mrisc32-a1/pipeline/pipeline_tb_prg.s
+++ mrisc32-a1/pipeline/pipeline_tb_prg.s
@@ -6,9 +6,70 @@
 ; -------------------------------------------------------------------------------------------------
 
 boot:
-    ; Start by setting up the stack.
+    ; Start by setting up the stack and clearing the registers.
     ldi     sp, 0x00020000  ; We grow down from 128KB.
+    cpuid   vl, z, z
+    ldi     s1, 0
+    ldi     s2, 0
+    ldi     s3, 0
+    ldi     s4, 0
+    ldi     s5, 0
+    ldi     s6, 0
+    ldi     s7, 0
+    ldi     s8, 0
+    ldi     s9, 0
+    ldi     s10, 0
+    ldi     s11, 0
+    ldi     s12, 0
+    ldi     s13, 0
+    ldi     s14, 0
+    ldi     s15, 0
+    ldi     s16, 0
+    ldi     s17, 0
+    ldi     s18, 0
+    ldi     s19, 0
+    ldi     s20, 0
+    ldi     s21, 0
+    ldi     s22, 0
+    ldi     s23, 0
+    ldi     s24, 0
+    ldi     s25, 0
+    ldi     s26, 0
+    ldi     s27, 0
+    ldi     s30, 0
+    ldi     v1, 0
+    ldi     v2, 0
+    ldi     v3, 0
+    ldi     v4, 0
+    ldi     v5, 0
+    ldi     v6, 0
+    ldi     v7, 0
+    ldi     v8, 0
+    ldi     v9, 0
+    ldi     v10, 0
+    ldi     v11, 0
+    ldi     v12, 0
+    ldi     v13, 0
+    ldi     v14, 0
+    ldi     v15, 0
+    ldi     v16, 0
+    ldi     v17, 0
+    ldi     v18, 0
+    ldi     v19, 0
+    ldi     v20, 0
+    ldi     v21, 0
+    ldi     v22, 0
+    ldi     v23, 0
+    ldi     v24, 0
+    ldi     v25, 0
+    ldi     v26, 0
+    ldi     v27, 0
+    ldi     v28, 0
+    ldi     v29, 0
+    ldi     v30, 0
+    ldi     v31, 0
 
+main:
     bl      mandelbrot
     bl      vector_flip
     b       exit
