2af08b08405af11b235bad0781e2ca6ea3cda43d
m <m@bitsnbites.eu>
2019-01-22 22:55:12 +0100

    make_png: Update RAM ranges and improve the script

--- hw/mrisc32-a1/make_png.sh
+++ hw/mrisc32-a1/make_png.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-cat /tmp/mrisc32_pipeline_tb_ram.bin | tail -c 98304 | head -c 65536 | convert -size 256x256 -depth 8 gray:- /tmp/mrisc32_pipeline_tb.png
+
+GFX_START=32768
+GFX_W=256
+GFX_H=256
+GFX_SIZE=$((${GFX_W} * ${GFX_H}))
+
+if [ $# -eq 0 ] ; then
+    echo "Usage: $0 ram-dump-file"
+    exit 1
+fi
+
+RAM_FILE="$1"
+PNG_FILE="${RAM_FILE}.png"
+
+cat "${RAM_FILE}" | tail -c +$((${GFX_START} + 1)) | head -c ${GFX_SIZE} | convert -size ${GFX_W}x${GFX_H} -depth 8 gray:- "${PNG_FILE}"
+
