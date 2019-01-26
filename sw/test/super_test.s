8cbc0250bcddb7e463cd2d8840f43d722a696409
m <m@bitsnbites.eu>
2019-01-26 15:15:24 +0100

    super_test: Work around bug in A1 branching logic
    See #71

--- sw/test/super_test.s
+++ sw/test/super_test.s
@@ -1069,6 +1069,14 @@ test_operand_forwarding:
     add     s25, s25, #96
     b       #check_results
 
+    ; TODO(m): Without these nop:s, somthing goes wrong in the branching logic and the A1 simulation
+    ; ends before the program is finished.
+    ; See: https://github.com/mbitsnbites/mrisc32/issues/71
+    nop
+    nop
+    nop
+    nop
+
 test_operand_forwarding_correct_results:
     .word   24
     .word   0x2468acf0, 0x2468acf0, 0x2468acf0, 0x2468acf0
