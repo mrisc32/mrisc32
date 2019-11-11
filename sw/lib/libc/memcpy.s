aea2565295fb26eb23cbeb4cf8d378693bb63679
m <m@bitsnbites.eu>
2019-11-11 18:57:07 +0100

    libc: Optimize memcpy

--- sw/lib/libc/memcpy.s
+++ sw/lib/libc/memcpy.s
@@ -33,7 +33,7 @@
 ;   s1: Copy of the input argument dest (s1)
 ;
 ; Clobbered scratch registers:
-;   s11, s12, s13, s14, s15, v15
+;   s11, s12, s13, s14, v15
 ; ----------------------------------------------------------------------------
 
     .text
@@ -47,40 +47,39 @@ memcpy:
     mov     s11, s1         ; s11 = dest (we need to preserve s1)
 
     ; Is the length long enough to bother with optizations?
-    sltu    s15, s3, #24
-    bs      s15, slow
+    sltu    s14, s3, #24
+    bs      s14, slow
 
     ; Are src and dest equally aligned (w.r.t 4-byte boundaries).
-    and     s14, s11, #3
-    and     s15, s2, #3
-    seq     s15, s14, s15
-    bns     s15, slow       ; Use the slow case unless equally aligned.
+    and     s13, s11, #3
+    and     s14, s2, #3
+    seq     s14, s13, s14
+    bns     s14, slow       ; Use the slow case unless equally aligned.
 
-    ; Do we need to do an initial alignment loop?
-    bz      s14, aligned
+    ; Do we need to align before the main loop?
+    bz      s13, aligned
 
     ; Align: Do a 1-3 bytes copy via a vector register, and adjust the memory
     ; pointers and the count.
-    sub     vl, #4, s14    ; vl = bytes left until aligned.
-    ldb     v15, s2, #1
+    sub     vl, #4, s13    ; vl = bytes left until aligned.
     sub     s3, s3, vl
+    ldb     v15, s2, #1
     add     s2, s2, vl
     stb     v15, s11, #1
     add     s11, s11, vl
 
 aligned:
     ; Vectorized word-copying loop.
-    lsr     s15, s3, #2     ; s15 > 0 due to earlier length requirement.
-    cpuid   s14, z, z       ; s14 = max vector length.
+    lsr     s14, s3, #2     ; s14 > 0 due to earlier length requirement.
+    cpuid   s13, z, z       ; s13 = max vector length.
 aligned_loop:
-    min     vl, s14, s15
-    sub     s15, s15, vl
+    min     vl, s13, s14
+    sub     s14, s14, vl
     ldw     v15, s2, #4
-    lsl     s13, vl, #2     ; Possibly do this before the loop.
     stw     v15, s11, #4
-    add     s2, s2, s13
-    add     s11, s11, s13
-    bnz     s15, aligned_loop
+    ldea    s2, s2, vl*4
+    ldea    s11, s11, vl*4
+    bnz     s14, aligned_loop
 
     ; Check how many bytes are remaining.
     and     vl, s3, #3      ; vl = bytes left after the aligned loop.
@@ -108,9 +107,9 @@ done:
 slow:
     ; Simple vectorized byte-copy loop (this is typically 4x slower than a
     ; word-copy loop).
-    cpuid   s14, z, z       ; s14 = max vector length.
+    cpuid   s13, z, z       ; s13 = max vector length.
 slow_loop:
-    min     vl, s14, s3
+    min     vl, s13, s3
     sub     s3, s3, vl
     ldb     v15, s2, #1
     add     s2, s2, vl
