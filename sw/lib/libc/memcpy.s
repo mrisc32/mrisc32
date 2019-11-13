5f9c084304f0a2c0e1c053d26d51c9c678b45099
m <m@bitsnbites.eu>
2019-11-13 09:41:38 +0100

    libc: Minor memcpy cleanups
    Allocate registers more similar to how GCC would do it, and re-arrange
    some instructions for slightly better performance in some cases.

--- sw/lib/libc/memcpy.s
+++ sw/lib/libc/memcpy.s
@@ -1,6 +1,6 @@
 ; -*- mode: mr32asm; tab-width: 4; indent-tabs-mode: nil; -*-
 ; ----------------------------------------------------------------------------
-; Copyright (c) 2018 Marcus Geelnard
+; Copyright (c) 2018-2019 Marcus Geelnard
 ;
 ; This software is provided 'as-is', without any express or implied warranty.
 ; In no event will the authors be held liable for any damages arising from the
@@ -33,69 +33,69 @@
 ;   s1: Copy of the input argument dest (s1)
 ;
 ; Clobbered scratch registers:
-;   s11, s12, s13, s14, v15
+;   s2, s3, s4, s5, s6, s7, v1
 ; ----------------------------------------------------------------------------
 
     .text
     .globl  memcpy
 memcpy:
-    mov     s12, vl         ; Preserve vl (it's a callee-saved register).
-
     ; Nothing to do?
-    bz      s3, done
+    bz      s3, exit
 
-    mov     s11, s1         ; s11 = dest (we need to preserve s1)
+    mov     s5, vl          ; Preserve vl (it's a callee-saved register).
+    mov     s4, s1          ; s4 = dest (we need to preserve s1)
 
     ; Is the length long enough to bother with optizations?
-    sltu    s14, s3, #24
-    bs      s14, slow
+    sltu    s7, s3, #24
+    bs      s7, slow
 
     ; Are src and dest equally aligned (w.r.t 4-byte boundaries).
-    and     s13, s11, #3
-    and     s14, s2, #3
-    seq     s14, s13, s14
-    bns     s14, slow       ; Use the slow case unless equally aligned.
+    and     s6, s4, #3
+    and     s7, s2, #3
+    seq     s7, s6, s7
+    bns     s7, slow        ; Use the slow case unless equally aligned.
 
     ; Do we need to align before the main loop?
-    bz      s13, aligned
+    bz      s6, aligned
 
     ; Align: Do a 1-3 bytes copy via a vector register, and adjust the memory
     ; pointers and the count.
-    sub     vl, #4, s13    ; vl = bytes left until aligned.
+    sub     vl, #4, s6      ; vl = bytes left until aligned.
     sub     s3, s3, vl
-    ldb     v15, s2, #1
+    ldb     v1, s2, #1
     add     s2, s2, vl
-    stb     v15, s11, #1
-    add     s11, s11, vl
+    stb     v1, s4, #1
+    add     s4, s4, vl
 
 aligned:
     ; Vectorized word-copying loop.
-    lsr     s14, s3, #2     ; s14 > 0 due to earlier length requirement.
-    cpuid   s13, z, z       ; s13 = max vector length.
+    lsr     s7, s3, #2      ; s7 > 0 due to earlier length requirement.
+    cpuid   s6, z, z        ; s6 = max vector length.
 aligned_loop:
-    min     vl, s13, s14
-    sub     s14, s14, vl
-    ldw     v15, s2, #4
-    stw     v15, s11, #4
+    min     vl, s6, s7
+    sub     s7, s7, vl
+    ldw     v1, s2, #4
     ldea    s2, s2, vl*4
-    ldea    s11, s11, vl*4
-    bnz     s14, aligned_loop
+    stw     v1, s4, #4
+    ldea    s4, s4, vl*4
+    bnz     s7, aligned_loop
 
     ; Check how many bytes are remaining.
     and     vl, s3, #3      ; vl = bytes left after the aligned loop.
     bz      vl, done
 
     ; Tail: Do a 1-3 bytes copy via a vector register.
-    ldb     v15, s2, #1
-    stb     v15, s11, #1
+    ldb     v1, s2, #1
+    stb     v1, s4, #1
 
 done:
-    ; Post vector-operation: Clear v15 (reg. length optimization).
+    ; Post vector-operation: Clear v1 (reg. length optimization).
     ldi     vl, #0
-    or      v15, vz, #0
+    or      v1, vz, #0
 
-    mov     vl, s12         ; Restore vl.
+    mov     vl, s5          ; Restore vl.
 
+exit:
     ; At this point s1 should contain it's original value (dest).
     j       lr
 
@@ -107,14 +107,14 @@ done:
 slow:
     ; Simple vectorized byte-copy loop (this is typically 4x slower than a
     ; word-copy loop).
-    cpuid   s13, z, z       ; s13 = max vector length.
+    cpuid   s6, z, z        ; s6 = max vector length.
 slow_loop:
-    min     vl, s13, s3
+    min     vl, s6, s3
     sub     s3, s3, vl
-    ldb     v15, s2, #1
+    ldb     v1, s2, #1
     add     s2, s2, vl
-    stb     v15, s11, #1
-    add     s11, s11, vl
+    stb     v1, s4, #1
+    add     s4, s4, vl
     bnz     s3, slow_loop
 
     b       done
