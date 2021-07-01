b6623a6302a01ecef1331add8b673846e7099df7
m <m@bitsnbites.eu>
2021-07-01 19:22:12 +0200

    isa-manual: Improve version logic in Makefile

--- isa-manual/get_version.sh
+++ isa-manual/get_version.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+TARGET_FILE=$1
+TMP_FILE=$(mktemp)
+
 # Generate a \specrev TeX command based on git describe.
-echo '\newcommand{\specrev}{\mbox{'"$(git describe --tags --match 'v*.*')"'}}'
+echo '\newcommand{\specrev}{\mbox{'"$(git describe --tags --match 'v*.*' --dirty=-MODIFIED)"'}}' > "${TMP_FILE}"
+
+# Only replace the target file if it's different from the temporary file.
+diff "${TMP_FILE}" "${TARGET_FILE}" &>/dev/null || cp "${TMP_FILE}" "${TARGET_FILE}"
+
+rm -f "${TMP_FILE}"
 
